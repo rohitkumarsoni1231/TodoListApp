c2ca501d94516c2673d9e296ec345fcf1b4dbab4
Rohit Kumar <oasisrohit470@gmail.com>
2023-11-27 14:06:38 +0400

    Initial Commit

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Rohit Kumar on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
